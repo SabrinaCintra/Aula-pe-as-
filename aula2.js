736aa93ed4574183e51b2e071d01b92d74728fcf
SabrinaCintra <sabrinacintra234@gmail.com>
2021-09-13 16:45:04 -0300

    first commit

---
+++ aula2.js
@@ -0,0 +1,35 @@
+
+
+
+ 
+console.log("quantidade de caracteres")
+if (peças.length >=100 ){
+    console.log("Produtos cadastrados ")
+}
+
+    console.log("quantidade de caracteres")
+if (peças.length <=3){
+    console.log("informar um erro")
+}
+
+console.log("quantidade de caracteres")
+if (peças.length >=10){
+    console.log(" Esta lista de prdutos não tem capacidade suficiente")
+} 
+
+    for (let index = 0 ; index < peças.length; index ++){
+    }
+    const atual = peças [index];
+    if (atual.length >100){
+        console.log(atual + " : se a peça possuir um peso superior a 100 pode cadastrar ")
+    }
+
+    if (atual.lengt >10)
+        console.log(atual + ": se a lista de peças possuir um peso superior a 10 nao tem capacidade suficiente")
+
+         else {console.log(atual + " : se a peça tenha um nome com quantidade inferior a 3 caracteres, informar erro ")
+    }
+
+
+
+
